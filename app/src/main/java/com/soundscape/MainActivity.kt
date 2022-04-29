5576970e959bcf9081355b683dab3666f2b195e7
FridtjofS <93829965+FridtjofS@users.noreply.github.com>
2022-04-29 10:53:18 +0200

    Basic Spotify Login, no data retrieved yet

--- app/src/main/java/com/soundscape/MainActivity.kt
+++ app/src/main/java/com/soundscape/MainActivity.kt
@@ -2,10 +2,32 @@ package com.soundscape
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.spotify.sdk.android.auth.AuthorizationClient
+import com.spotify.sdk.android.auth.AuthorizationRequest
+import com.spotify.sdk.android.auth.AuthorizationResponse
+import com.soundscape.infrastructure.SpotifyConstants
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spotify_login_btn = findViewById<Button>(R.id.spotify_login_btn)
+
+        spotify_login_btn.setOnClickListener {
+            val builder: AuthorizationRequest.Builder = AuthorizationRequest.Builder(
+                SpotifyConstants.CLIENT_ID,
+                AuthorizationResponse.Type.TOKEN,
+                SpotifyConstants.REDIRECT_URI)
+
+            val request: AuthorizationRequest = builder.build()
+
+            AuthorizationClient.openLoginActivity(
+                this,
+                SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
+                request
+            )
+        }
     }
 }
---
+++ app/src/main/java/com/soundscape/infrastructure/SpotifyConstants.kt
@@ -0,0 +1,7 @@
+package com.soundscape.infrastructure
+
+object SpotifyConstants {
+    val CLIENT_ID = "1cbdab2be2074956a3c801e859908c86"
+    val AUTH_TOKEN_REQUEST_CODE = 1312
+    val REDIRECT_URI = "soundscape://"
+}
