019b7128f8b8c383e63febd68305fe424fb60d0e
erik <erik_se@posteo.de>
2022-05-02 15:23:13 +0200

    composable spotify login and continue button

--- app/src/main/java/com/soundscape/MainActivity.kt
+++ app/src/main/java/com/soundscape/MainActivity.kt
@@ -1,33 +1,88 @@
 package com.soundscape
 
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.content.ContextWrapper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
 import com.soundscape.infrastructure.SpotifyConstants
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            // TODO: Add a theme for consistent colouring
+            Surface(color = MaterialTheme.colors.background) {}
 
-        val spotify_login_btn = findViewById<Button>(R.id.spotify_login_btn)
-
-        spotify_login_btn.setOnClickListener {
-            val builder: AuthorizationRequest.Builder = AuthorizationRequest.Builder(
-                SpotifyConstants.CLIENT_ID,
-                AuthorizationResponse.Type.TOKEN,
-                SpotifyConstants.REDIRECT_URI)
-
-            val request: AuthorizationRequest = builder.build()
-
-            AuthorizationClient.openLoginActivity(
-                this,
-                SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
-                request
-            )
         }
     }
 }
+
+
+fun Context.findActivity(): ComponentActivity? = when (this) {
+    is ComponentActivity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
+
+fun spotifyLogin(context: Context) {
+    val activity = context.findActivity()
+
+    val builder: AuthorizationRequest.Builder = AuthorizationRequest.Builder(
+        SpotifyConstants.CLIENT_ID,
+        AuthorizationResponse.Type.TOKEN,
+        SpotifyConstants.REDIRECT_URI
+    )
+
+    val request: AuthorizationRequest = builder.build()
+
+    AuthorizationClient.openLoginActivity(
+        activity,
+        SpotifyConstants.AUTH_TOKEN_REQUEST_CODE,
+        request
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SoundScapePreview(){
+    Surface()
+}
+
+@Composable
+fun Surface() {
+    LoginWithSpotifyButton()
+    ContinueWithLocalButton()
+}
+
+@Composable
+fun LoginWithSpotifyButton() {
+    val context = LocalContext.current
+    Button(
+        onClick = { spotifyLogin(context) })
+    {
+        Text("Login With Spotify")
+    }
+}
+
+@Composable
+fun ContinueWithLocalButton() {
+    Button(onClick = { /*TODO*/ })
+    {
+        Text(text = "Continue")
+    }
+}
+
