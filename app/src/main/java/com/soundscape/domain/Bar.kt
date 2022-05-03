dbe5562304528d8366a939ecb78dce0122e2f973
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2022-05-03 22:08:04 +0200

    Simplify serialization of bars

--- app/src/main/java/com/soundscape/domain/Bar.kt
+++ app/src/main/java/com/soundscape/domain/Bar.kt
@@ -1,12 +1,15 @@
 package com.soundscape.domain
-import org.json.*
+import kotlinx.serialization.Serializable
 
+@Serializable
 class Bar(
     val type: String,
     val id: Long,
-    val name: String,
     //val openingHours: String?,
     val lat: Double,
     val lon: Double,
-    val tags: JSONObject,
-    val geohash: String)
+    val tags: HashMap<String, String>,
+    val geohash: String
+) {
+    val name get() = this.tags["name"] ?: "NoName";
+}
--- app/src/main/java/com/soundscape/infrastructure/Serialize.kt
+++ app/src/main/java/com/soundscape/infrastructure/Serialize.kt
@@ -2,34 +2,11 @@ package com.soundscape.infrastructure
 
 import android.content.Context
 import com.soundscape.domain.Bar
-import org.json.JSONArray
-import org.json.JSONTokener
-import java.util.ArrayList
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 
-fun getBarList(context: Context) : ArrayList<Bar> {
-    val data = context.assets.open("bars_tuebingen.jsonl").bufferedReader().readText()
-    val jsonBars = JSONTokener(data).nextValue() as JSONArray
-    val barList : ArrayList<Bar> = arrayListOf<Bar>()
-
-    for (i in 0 until jsonBars.length()) {
-
-        val type = jsonBars.getJSONObject(i).getString("type")
-        val id = jsonBars.getJSONObject(i).getString("id").toLong()
-        val lat = jsonBars.getJSONObject(i).getString("lat").toDouble()
-        val lon = jsonBars.getJSONObject(i).getString("lon").toDouble()
-
-        // go into nested structure
-        val tags = jsonBars.getJSONObject(i).getJSONObject("tags")
-        val name = tags.getString("name")
-        /* TODO: Implement opening hours
-        val openingHours = try {
-            tags.getString("opening_hours")
-        } catch (e: NoSuchFieldError ) { null }
-
-         */
-        val geohash = jsonBars.getJSONObject(i).getString("geohash")
-
-        barList.add(i, Bar(type, id, name, lat, lon, tags, geohash))
-    }
-    return barList
+fun getBarList(context: Context) : List<Bar> {
+    val text = context.assets.open("bars_tuebingen.jsonl").bufferedReader().readText()
+    val lines = text.split("\r?\n|\r".toRegex()).toTypedArray()
+    return lines.map { Json.decodeFromString(it) }
 }
