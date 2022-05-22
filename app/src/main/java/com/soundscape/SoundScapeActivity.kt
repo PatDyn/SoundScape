19e841b513bbba83b1d64ec6027781e3d5f5675c
Lennard Berger <3730888+Fohlen@users.noreply.github.com>
2022-05-22 13:03:46 +0100

    Request location on app start

--- app/src/main/java/com/soundscape/SoundScapeActivity.kt
+++ app/src/main/java/com/soundscape/SoundScapeActivity.kt
@@ -1,39 +1,70 @@
 package com.soundscape
 
-import android.app.PendingIntent.getActivity
-import android.content.Context
-import android.content.ContextWrapper
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import ch.hsr.geohash.GeoHash
 import com.example.simplewellness.ui.theme.SoundScapeTheme
-import com.spotify.sdk.android.auth.AuthorizationClient
-import com.spotify.sdk.android.auth.AuthorizationRequest
-import com.spotify.sdk.android.auth.AuthorizationResponse
-import com.soundscape.infrastructure.SpotifyConstants
-import com.soundscape.infrastructure.findActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.soundscape.userinterface.LoginBody
 import com.soundscape.userinterface.MainBody
 import com.soundscape.userinterface.StartOffBody
 
 class MainActivity : ComponentActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var locationHash: String? = null
+
+    // TODO: This should be used to filter locations using:
+    // locations.filter(location -> location.hash.startsWith(hash))
+    fun getLocationHash(): String? {
+        return locationHash
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
+        fusedLocationClient.lastLocation
+            .addOnSuccessListener { location : Location? ->
+                locationHash = location?.let {
+                    GeoHash.withCharacterPrecision(
+                        it.latitude,
+                        location.longitude,
+                        5
+                    ).toString()
+                };
+            }
+
         setContent {
             SoundScapeTheme {
                 Surface(
