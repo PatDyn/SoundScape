a150709e4ff7eac46afee5ae7608a1ad573401a9
erik <erik_se@posteo.de>
2022-06-28 15:33:03 +0200

    Implemented PKCE
    We now have Auth Code Flow + PKCE Implemented.
    Next Up: Using DataStore to save the access and refresh tokens.

--- app/src/main/java/com/soundscape/SpotifyActivity.kt
+++ app/src/main/java/com/soundscape/SpotifyActivity.kt
@@ -1,23 +1,27 @@
 package com.soundscape
 
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.StrictMode
 import android.os.StrictMode.ThreadPolicy
+import android.widget.Toast
 import com.soundscape.infrastructure.SpotifyConstants
+import com.soundscape.infrastructure.toHashed
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
 import se.michaelthelin.spotify.SpotifyApi
+import se.michaelthelin.spotify.enums.AuthorizationScope
 import se.michaelthelin.spotify.model_objects.specification.Paging
 import se.michaelthelin.spotify.model_objects.specification.Track
 import se.michaelthelin.spotify.model_objects.specification.User
+import se.michaelthelin.spotify.requests.authorization.authorization_code.pkce.AuthorizationCodePKCERequest
 import java.lang.Exception
+import java.net.URI
 
 
-/* ToDo: Implementing PKCE
+
+/* ToDo: Implementing Auth Code Flow
         need to add state for security
         for pkce add val code_challenge_method = "S256"
         code_challenge = hash(generate.random.string(), SHA256, length = 64)
@@ -46,15 +50,40 @@ import java.lang.Exception
 class SpotifyActivity : Activity() {
 
 
-    val STATE_STRING = SpotifyConstants.STATE
+    private val STATE_STRING = SpotifyConstants.STATE
+    private val CODE_VERIFIER = SpotifyConstants.CODE_VERIFIER
+    private val CODE_CHALLENGE = CODE_VERIFIER.toHashed("SHA-256")
 
     // build user authorization request
+/*
+    private val builder: AuthorizationCodeUriRequest.Builder = AuthorizationCodeUriRequest
+        .Builder()
+        .client_id(SpotifyConstants.CLIENT_ID)
+        .response_type(SpotifyConstants.RESPONSE_TYPE)
+        .redirect_uri(URI(SpotifyConstants.REDIRECT_URI))
+        .state(STATE_STRING)
+        .code_challenge_method(SpotifyConstants.CODE_CHALLENGE_METHOD)
+        .code_challenge(CODE_CHALLENGE)
+        .scope(AuthorizationScope.USER_READ_PRIVATE, AuthorizationScope.USER_TOP_READ)
+
+ */
+
     private val builder: AuthorizationRequest.Builder = AuthorizationRequest.Builder(
         SpotifyConstants.CLIENT_ID,
         SpotifyConstants.RESPONSE_TYPE, // needs to be .CODE
-        SpotifyConstants.REDIRECT_URI)
+        SpotifyConstants.REDIRECT_URI
+    )
         .setState(STATE_STRING)
         .setScopes(Array(1) { "user-read-private,user-top-read" })
+        .setCustomParam(
+            "code_challenge_method",
+            SpotifyConstants.CODE_CHALLENGE_METHOD
+        )
+        .setCustomParam(
+            "code_challenge",
+            CODE_CHALLENGE
+        )
+
 
     /* user-read private, for profile pic and name,
     *  user-top-read, gets top tracks (or artists, up to 50) from all time,
@@ -64,6 +93,7 @@ class SpotifyActivity : Activity() {
 
         super.onStart()
 
+        /*
         //initial spotify token
         var SPOTIFY_TOKEN: String = ""
 
@@ -86,6 +116,8 @@ class SpotifyActivity : Activity() {
         }
 
         loadToken()
+        */
+
         //TODO if (SPOTIFY_TOKEN == "") bla bla open login activity ( "" is default value)
 
         val policy = ThreadPolicy.Builder().permitNetwork().build()
@@ -108,31 +140,60 @@ class SpotifyActivity : Activity() {
         val uri = intent?.data
         val response = AuthorizationResponse.fromUri(uri)
 
-        when (response.type) {
-            AuthorizationResponse.Type.CODE -> { // if CODE exchange for access token
-                val spotifyApi = SpotifyApi.Builder()
-                    .setAccessToken(response.accessToken)
-                    .build()
+        when (response.state) {
+            STATE_STRING -> {
+                when (response.type) {
+                    AuthorizationResponse.Type.CODE -> {
 
-                // TODO: Make this async
-                val topTracksRequest = spotifyApi.usersTopTracks.build()
-                try {
-                    val tracks: Paging<Track> = topTracksRequest.execute()
-                    intent?.putExtra("tracks", tracks)
-                } catch (e: Exception) {
-                    println(e.message)
-                }
+                        val authCodePKCECredentials = AuthorizationCodePKCERequest.Builder()
+                            .grant_type("authorization_code")
+                            .code(response.code)
+                            .redirect_uri(URI(SpotifyConstants.REDIRECT_URI))
+                            .client_id(SpotifyConstants.CLIENT_ID)
+                            .code_verifier(CODE_VERIFIER)
+                            .build()
+                            .execute()
 
-                val userProfileRequest = spotifyApi.currentUsersProfile.build()
-                try {
-                    val user: User = userProfileRequest.execute()
-                    intent?.putExtra("user", user);
-                } catch (e: Exception) {
-                    println(e.message)
+                        val spotifyApi = SpotifyApi.Builder()
+                            .setAccessToken(authCodePKCECredentials.accessToken)
+                            .build()
+
+                        // TODO: Make this async
+                        val topTracksRequest = spotifyApi.usersTopTracks.build()
+                        try {
+                            val tracks: Paging<Track> = topTracksRequest.execute()
+                            intent?.putExtra("tracks", tracks)
+                        } catch (e: Exception) {
+                            println(e.message)
+                        }
+
+                        val userProfileRequest = spotifyApi.currentUsersProfile.build()
+                        try {
+                            val user: User = userProfileRequest.execute()
+                            intent?.putExtra("user", user);
+                        } catch (e: Exception) {
+                            println(e.message)
+                        }
+                    } // TODO: handle Response.Type.ERROR
+                    AuthorizationResponse.Type.ERROR -> {
+                        Toast.makeText(
+                            this,
+                            "Error_Spotify_Authorization_Response_was_error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    else -> {
+
+                    }
                 }
             }
-            AuthorizationResponse.Type.ERROR -> {}
-            else -> {}
+            null -> {
+                Toast.makeText(
+                    this,
+                    "Error_Spotify_Response_State_was_null",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         }
         finish()
     }
--- app/src/main/java/com/soundscape/infrastructure/SpotifyConstants.kt
+++ app/src/main/java/com/soundscape/infrastructure/SpotifyConstants.kt
@@ -4,18 +4,21 @@ import com.spotify.sdk.android.auth.AuthorizationResponse
 
 object SpotifyConstants {
 
-    val AUTH_TOKEN_REQUEST_CODE = System.getenv("SPOTIFY_AUTH_TOKEN_REQUEST_CODE") ?: 1234
+    const val AUTH_TOKEN_REQUEST_CODE : Int =  1234
+    const val spotifyTokenURL : String = "https://accounts.spotify.com/api/token"
 
     // TODO: we need to protect the client ID
     // Auth Code Flow
     val CLIENT_ID = System.getenv("SPOTIFY_CLIENT_ID") ?: "8129e610de1c4ab19f943e291750d6da"
     val RESPONSE_TYPE = AuthorizationResponse.Type.CODE
+    //val RESPONSE_TYPE = "code"
     const val REDIRECT_URI = "soundscape://callback"
     val STATE = getRandomString(16).toHashed("SHA-256")
 
     // PKCE
     const val CODE_CHALLENGE_METHOD = "S256"
-    val CODE_CHALLENGE = getRandomString(64).toHashed("SHA-256")
+    val CODE_VERIFIER = getRandomString(64)
+
 }
 
 
